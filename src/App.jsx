1caed1dfc20a1fb20e47afeb01ddf499b496f5e3
Paige Hoffman <paige.hoffman@att.net>
2026-08-16 02:39:30 -0500

    Minor file name refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Dashboard } from './Pages/Dashboard.jsx'
 import { Home } from './Pages/Home.jsx'
 import { Log } from './Pages/Log.jsx'
 import { About } from './Pages/About.jsx'
-import { FormSettings } from './Pages/FormSettings.jsx'
+import { Setup } from './Pages/Setup.jsx'
 import { HashRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
           <Route path ="/Dashboard" element={<Dashboard/>}/>
           <Route path ="/Log" element={<Log/>}/>
           <Route path ="/About" element={<About/>}/>
-          <Route path ="/FormSettings" element={<FormSettings/>}/>
+          <Route path ="/Setup" element={<Setup/>}/>
         </Routes>
       </HashRouter>
     </>
